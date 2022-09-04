cd067ca0947d9c0a42938499037d9e72e567bc23
NonamePoc <pavloshaban@gmail.com>
2022-09-04 13:04:55 +0300

    V2 version

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const http = require('http');
+const  Module = require("./module")
 
-const hostname = '127.0.0.1';
-const port = 3000;
+
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+   const  _module = new Module.Module("Pasha");
+
+   res.write(_module.getMessage());
+   res.end();
+
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(5000);
